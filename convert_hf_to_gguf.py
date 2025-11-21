74324804d7164c5ebb1a9629bbb7f67a2657c452
Cyrus_CC <108733849+Cbyernus@users.noreply.github.com>
2025-11-21 18:01:21 +0800

    Clean up imports in convert_hf_to_gguf.py
    Removed unused imports and error handling for mistral_common.

--- convert_hf_to_gguf.py
+++ convert_hf_to_gguf.py
@@ -28,31 +28,6 @@ if TYPE_CHECKING:
 if 'NO_LOCAL_GGUF' not in os.environ:
     sys.path.insert(1, str(Path(__file__).parent / 'gguf-py'))
 import gguf
-from gguf.vocab import MistralTokenizerType, MistralVocab
-
-try:
-    from mistral_common.tokens.tokenizers.base import TokenizerVersion # pyright: ignore[reportMissingImports]
-    from mistral_common.tokens.tokenizers.multimodal import DATASET_MEAN as _MISTRAL_COMMON_DATASET_MEAN, DATASET_STD as _MISTRAL_COMMON_DATASET_STD # pyright: ignore[reportMissingImports]
-    from mistral_common.tokens.tokenizers.tekken import Tekkenizer # pyright: ignore[reportMissingImports]
-    from mistral_common.tokens.tokenizers.sentencepiece import ( # pyright: ignore[reportMissingImports]
-        SentencePieceTokenizer,
-    )
-
-    _mistral_common_installed = True
-    _mistral_import_error_msg = ""
-except ImportError:
-    _MISTRAL_COMMON_DATASET_MEAN = (0.48145466, 0.4578275, 0.40821073)
-    _MISTRAL_COMMON_DATASET_STD = (0.26862954, 0.26130258, 0.27577711)
-
-    _mistral_common_installed = False
-    TokenizerVersion = None
-    Tekkenizer = None
-    SentencePieceTokenizer = None
-    _mistral_import_error_msg = (
-        "Mistral format requires `mistral-common` to be installed. Please run "
-        "`pip install mistral-common[image,audio]` to install it."
-    )
-
 
 logger = logging.getLogger("hf-to-gguf")
 
